fbf5cb75e04844a5a0be9c86aabdc6720380774d
a50017567 <anbochao@huawei.com>
2021-04-06 17:06:38 +0800

    编写多路由的消息处理demo

---
+++ demo/ZinxV0.6/Server.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"trueabc.top/zinx/ziface"
+	"trueabc.top/zinx/znet"
+)
+
+// ping test 自定義路由
+type PingRouter struct {
+	znet.BaseRouter
+}
+
+func (pr *PingRouter) Handler(request ziface.IRequest) {
+	fmt.Println("Call Router handler")
+	// 先读取客户端的数据, 再ping... ping... ping...
+	fmt.Printf("recv from client: MsgId= %d, data=%s", request.GetMsgID(), request.GetData())
+
+	err := request.GetConnection().SendMsg(200, []byte("ping router is working"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+type HelloRouter struct {
+	znet.BaseRouter
+}
+
+func (pr *HelloRouter) Handler(request ziface.IRequest) {
+	fmt.Println("Call Hello handler")
+	// 先读取客户端的数据, 再ping... ping... ping...
+	fmt.Printf("recv from client: MsgId= %d, data=%s", request.GetMsgID(), request.GetData())
+
+	err := request.GetConnection().SendMsg(201, []byte("hello hello hello zinx"))
+	if err != nil {
+		fmt.Println(err)
+	}
+}
+
+func main() {
+	s := znet.NewServer("[zinx 0.6]")
+
+	s.AddRouter(0, &PingRouter{})
+	s.AddRouter(1, &HelloRouter{})
+	s.Serve()
+}
