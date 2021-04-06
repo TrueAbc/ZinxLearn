8049b528968f08d136485bcff12fee342e5a9e80
a50017567 <anbochao@huawei.com>
2021-04-06 14:33:27 +0800

    定义消息体结构

---
+++ ziface/imessage.go
@@ -0,0 +1,15 @@
+package ziface
+
+/*
+ 将请求的消息封装到一个Message中，定义抽象的接口
+*/
+
+type IMessage interface {
+	GetMsgId() uint32
+	GetMsgLen() uint32
+	GetMsg() []byte
+
+	SetMsgId(uint32)
+	SetMsgLen(uint32)
+	SetMsg([]byte)
+}
